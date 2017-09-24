80b75cdc49d004bb15c4e401f838a229c157ae77
KLD0093 <KLD0093@hotmail.com>
2017-09-23 22:23:44 -0400

    Added helper Inspecter button to align patterns  in a LinearWave

---
+++ Assets/Editor/LinearWaveEditor.cs
@@ -0,0 +1,32 @@
+﻿using ChainedRam.Alebi.Battle;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LinearWave))]
+public class LinearWaveEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Set up Patterns"))
+        {
+            LinearWave linearWave = (LinearWave)target;
+
+            int i= 0; 
+            foreach (Pattern p in linearWave.patterns)
+            {
+                if (p != null)
+                {
+                    linearWave.SetUpPattern(i++, p);
+                }
+            }
+
+        
+
+        }
+
+    }
+}
