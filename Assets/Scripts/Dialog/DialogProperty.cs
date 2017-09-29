e77670543f5966309e43de6f75aaa693fdc6a177
KLD0093 <KLD0093@hotmail.com>
2017-09-28 20:42:18 -0400

    Implemented basic dialog system

---
+++ Assets/Scripts/Dialog/DialogProperty.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DialogProperty : int
+{
+    None = 0,
+    PauseAtEnd = 1,
+    PauseOnNewLine = 2,
+    PauseOnSpace = 4,
+
+
+
+}
---
+++ Assets/Scripts/Dialog/DialogPropertyExtentions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class DialogPropertyExtentions
+{
+   
+    public static bool HasFlag(this DialogProperty property, DialogProperty flag)
+    {
+        return (property & flag) == flag; 
+    }
+
+    public static void AddFlag(this DialogProperty property, DialogProperty flag)
+    {
+        property = property | flag; 
+    }
+
+    public static void RemoveFlag(this DialogProperty property, DialogProperty flag)
+    {
+        property  =  property & ~flag;
+    }
+}
