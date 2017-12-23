e4a51a7c28e7033e63403f2fdf409d1997e29297
KLD <kld0093@hotmail.com>
2017-12-23 10:55:13 -0500

    Added DialogPropertyController
    Major:
    [DialogPropertyController]
    - Enables setting DialogPause flags.
    
    Minor:
    [DialogPropertyExtention]
    - Now called EnumExtentions, and it's cool.
    
    [DialogPauseProperty]
    - Renamed file name to reflect enum name.

---
+++ Assets/Scripts/Core/Dialog/DialogPropertyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Allows changing <see cref="Dialog.Property"/> in inspecter. Requires a DialogSegment Component
+/// </summary>
+public class DialogPropertyController : MonoBehaviour
+{
+    public DialogSegment Segment; 
+
+    public bool Space;
+    public bool NewLine;
+    public bool PageEnd;
+    public bool NewPage;
+
+    void Update()
+    {
+        EnumExtensions.SetFlag(ref Segment.property, DialogPauseProperty.Space, Space );
+        EnumExtensions.SetFlag(ref Segment.property, DialogPauseProperty.NewLine, NewLine);
+        EnumExtensions.SetFlag(ref Segment.property, DialogPauseProperty.PageEnd, PageEnd);
+        EnumExtensions.SetFlag(ref Segment.property, DialogPauseProperty.NewPage, NewPage);
+    }
+
+    private void OnValidate()
+    {
+        if(Segment == null)
+        {
+            Segment = GetComponent<DialogSegment>();
+
+            if(Segment == null)
+            {
+                Debug.LogError("Missing DialogSegment component.", this); 
+            }
+        }
+    }
+
+}
