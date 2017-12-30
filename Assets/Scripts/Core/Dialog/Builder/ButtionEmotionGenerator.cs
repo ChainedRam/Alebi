2f0f8312db4d957b02b9382eb1827be21204e00c
ChainedRam <chainedram@gmail.com>
2017-12-29 23:47:10 -0500

    Added facial animation creator scnene

---
+++ Assets/Scripts/Core/Dialog/Builder/ButtionEmotionGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtionEmotionGenerator : MonoBehaviour
+{
+    public EmotionalCharachter Character;
+
+    public Button ButtonPrefab;
+    public GameObject ButtonParent;
+
+    private string HoldCharacter = ""; 
+
+    private void Start()
+    {
+        foreach(var clip in Character.clips)
+        {
+            string emotionName = clip.name.Substring(clip.name.LastIndexOf('_') + 1); //WRONG 
+
+            Button plsAddMe = Instantiate(ButtonPrefab, ButtonParent.transform);
+            plsAddMe.GetComponentInChildren<Text>().text = emotionName;
+            plsAddMe.onClick.AddListener(() => { Character.SetEmotion(emotionName.ToLower() + HoldCharacter);});
+            plsAddMe.gameObject.SetActive(true); 
+        }
+    }
+
+    public void TurnHoldOn()
+    {
+        HoldCharacter = "+"; 
+    }
+
+    public void TurnHoldOff()
+    {
+        HoldCharacter = "";
+        Character.Release(); 
+    }
+}
