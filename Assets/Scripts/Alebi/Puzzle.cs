2412294cf1bee23f2da9075381307821713bbbdb
KLD <KLD0093@hotmail.com>
2018-02-10 10:59:33 +0300

    Example
    Description
    
    closes #6

--- Assets/Scripts/Alebi/Puzzle.cs
+++ Assets/Scripts/Alebi/Puzzle.cs
@@ -5,69 +5,69 @@ using UnityEngine;
 
 public enum TileContent
 {
-    Empty, 
-    Box, 
-    Player
+	Empty, 
+	Box, 
+	Player
 }
 
 
 public class Puzzle : MonoBehaviour
 {
-    public GameObject[][] Board;
-    public TileContent[][] BoardContent; 
+	public GameObject[][] Board;
+	public TileContent[][] BoardContent; 
 
-    public int Width;
-    public int Height; 
+	public int Width;
+	public int Height; 
 
-    public Vector2 PlayerPosition;
+	public Vector2 PlayerPosition;
 
-    public Vector2[] BoxPositions;
+	public Vector2[] BoxPositions;
 
-    public GameObject Player;
+	public GameObject Player;
 
-    public GameObject BoxPrefab;
-    public GameObject TilePrefab;
+	public GameObject BoxPrefab;
+	public GameObject TilePrefab;
 
-    public Vector2 CellSize;
+	public Vector2 CellSize;
 
-    public int[][] BoxBoard; 
+	public int[][] BoxBoard; 
 
-    // Use this for initialization
-    void Start ()
-    {
-        float ix = (-CellSize.x / 2) * Width;
-        float iy = (-CellSize.y / 2) * Height;
+	// Use this for initialization
+	void Start ()
+	{
+		float ix = (-CellSize.x / 2) * Width;
+		float iy = (-CellSize.y / 2) * Height;
 
-        Board = new GameObject[Height][];
-        BoardContent = new TileContent[Height][];
-        for (int i = 0; i < Height; i++)
-        {
-            Board[i] = new GameObject[Width];
-            BoardContent[i] = new TileContent[Width]; 
-            for (int j = 0; j < Width; j++)
-            {
-                BoardContent[i][j] = TileContent.Empty; 
-                Board[i][j] = Instantiate(TilePrefab, this.transform);
-                Board[i][j].name =$"{i},{j}";
-                Board[i][j].transform.localPosition = new Vector3(ix + CellSize.x * j, iy + CellSize.y * i, 0);
-            }
-        }
+		Board = new GameObject[Height][];
+		BoardContent = new TileContent[Height][];
+		for (int i = 0; i < Height; i++)
+		{
+			Board[i] = new GameObject[Width];
+			BoardContent[i] = new TileContent[Width]; 
+			for (int j = 0; j < Width; j++)
+			{
+				BoardContent[i][j] = TileContent.Empty; 
+				Board[i][j] = Instantiate(TilePrefab, this.transform);
+				Board[i][j].name =$"{i},{j}";
+				Board[i][j].transform.localPosition = new Vector3(ix + CellSize.x * j, iy + CellSize.y * i, 0);
+			}
+		}
 
-        Player.transform.SetParent(Board[(int)PlayerPosition.y][(int)PlayerPosition.x].transform);
-        Player.transform.localPosition = Vector3.zero;
+		Player.transform.SetParent(Board[(int)PlayerPosition.y][(int)PlayerPosition.x].transform);
+		Player.transform.localPosition = Vector3.zero;
 
-        foreach (var boxPos in BoxPositions)
-        {
-            GameObject box = Instantiate(BoxPrefab, Board[(int)boxPos.y][(int)boxPos.x].transform);
-            BoardContent[(int)boxPos.y][(int)boxPos.x] = TileContent.Box;
-            box.name = $"box {boxPos.x},{boxPos.y}";
-            box.transform.localPosition = Vector3.zero; 
-        }
-    }
+		foreach (var boxPos in BoxPositions)
+		{
+			GameObject box = Instantiate(BoxPrefab, Board[(int)boxPos.y][(int)boxPos.x].transform);
+			BoardContent[(int)boxPos.y][(int)boxPos.x] = TileContent.Box;
+			box.name = $"box {boxPos.x},{boxPos.y}";
+			box.transform.localPosition = Vector3.zero; 
+		}
+	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
+	{
 		
 	}
 }
