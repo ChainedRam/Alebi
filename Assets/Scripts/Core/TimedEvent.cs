4b79318e07ec7835de6eef53638a7e316b3ca1a7
Monstring <monstring1@hotmail.com>
2018-02-19 20:58:06 +0300

    Created a class for the oil bottle projectile making the bottle disappear when the oil appears

---
+++ Assets/Scripts/Core/TimedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimedEvent : MonoBehaviour {
+
+    public UnityEvent onTrigger;
+    public float time;
+
+	void Start () {
+        StartCoroutine(timer());
+	}
+	
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(time);
+        onTrigger.Invoke();
+    }
+}
