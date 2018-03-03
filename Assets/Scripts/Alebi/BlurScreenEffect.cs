23aeb9ae4b40e75de94522d3e8e570231307c934
KLD <KLD0093@hotmail.com>
2018-03-03 10:32:51 +0300

    Fixed BlurScreen not setting values.

--- Assets/Scripts/Alebi/BlurScreenEffect.cs
+++ Assets/Scripts/Alebi/BlurScreenEffect.cs
@@ -10,43 +10,43 @@ public class BlurScreenEffect : MonoBehaviour, IStatusEffect
 {
 
 
-    private BlurOptimized blur;
+	private BlurOptimized blur;
 
-    private void Start()
-    {
-        blur = Camera.main.GetComponent<BlurOptimized>();
-        if (blur == null)
-        {
-            blur = Camera.main.gameObject.AddComponent<BlurOptimized>();
-            blur.downsample = 1;
-            blur.blurIterations = 2;
-            blur.blurSize = 3;
-            blur.enabled = false;
-        }
-    }
-    public void Show()
+	private void Start()
 	{
-        blur.enabled = true;
+		blur = Camera.main.GetComponent<BlurOptimized>();
+		if (blur == null)
+		{
+			blur = Camera.main.gameObject.AddComponent<BlurOptimized>();
+		}
+		blur.downsample = 1;
+		blur.blurIterations = 2;
+		blur.blurSize = 3;
+		blur.enabled = false;
+	}
+	public void Show()
+	{
+		blur.enabled = true;
 		gameObject.SetActive(true);
 	}
 
 	public void Hide()
 	{
-        blur.enabled = false;
-        gameObject.SetActive(false);
-    }
+		blur.enabled = false;
+		gameObject.SetActive(false);
+	}
 
-    #region  IStatusEffect Interface Property
-    public string Name
+	#region  IStatusEffect Interface Property
+	public string Name
 	{
 		get
 		{
 			return "Blur";
 		}
 	}
-    #endregion
-    #region  IStatusEffect Interface Methods
-    public void Init(Player p)
+	#endregion
+	#region  IStatusEffect Interface Methods
+	public void Init(Player p)
 	{
 		Show();
 	}
