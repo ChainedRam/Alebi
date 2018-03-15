a544d3b119a832775066c71656b47e135d621c60
KLD <KLD0093@hotmail.com>
2018-03-15 17:13:13 +0300

    Added Strech and Fade out behaviours to Sand Projectile

---
+++ Assets/Scripts/Core/SpriteFadeOut.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteFadeOut : MonoBehaviour
+{
+    public float Duration;
+
+    private SpriteRenderer Sprite;
+    private float? Speed; 
+
+    
+
+    public void Start ()
+    {
+        Sprite = GetComponent<SpriteRenderer>(); 
+    }
+     
+    public void StartFading()
+    {
+        enabled = true; 
+    }
+
+
+    void FixedUpdate ()
+    {
+        if(Speed == null)
+        {
+            Speed = Sprite.color.a / (Duration / Time.fixedDeltaTime);
+        }
+
+        //print(Sprite.color.a);
+        
+        Sprite.color = new Color(Sprite.color.r, Sprite.color.g, Sprite.color.b, Sprite.color.a - Speed.Value); 
+    }
+}
---
+++ Assets/Scripts/Core/StrechScale.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrechScale : MonoBehaviour
+{
+	public Vector3 StrechTo;
+
+	public float Speed;
+
+	private void Awake()
+	{
+		enabled = true; 
+	}
+
+	public void BeginStreching()
+	{
+		enabled = true; 
+	}
+
+	private void Update()
+	{
+		transform.localScale = Vector3.MoveTowards(transform.localScale, StrechTo, Speed); 
+	}
+}
