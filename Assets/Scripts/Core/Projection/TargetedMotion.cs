dc47c106208ff198ad9bad74043d6e5d37fde9ae
KLD <kld0093@hotmail.com>
2018-03-18 16:52:57 +0300

    Implemented TargetedMotion
    closes #31

---
+++ Assets/Scripts/Core/Projection/TargetedMotion.cs
@@ -0,0 +1,131 @@
+﻿using ChainedRam.Core.Extentions;
+using ChainedRam.Core.Projection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChainedRam.Core.Projection
+{
+    public class TargetedMotion : AbsoluteMotion
+    {
+        public GameObject Target;
+
+        [Range(1f, 350f)]
+        public float Degree = 1;
+
+        public float Speed;
+
+        public bool HideGizmo;
+
+        private const float StepSize = 0.005f;
+        private float DegreeToTarget;
+        private float DegreeStepLength;
+        private float DegreeStepSize;
+        private float StepCounter;
+
+        public override void Initialize(Projectile sender, float delta = 1)
+        {
+            base.Initialize(sender, delta);
+
+            FancyMath();
+        }
+
+        public override Vector2 GetRelativeOffset(Vector2 defaultVector)
+        {
+            if (StepCounter > DegreeStepLength)
+            {
+                return defaultVector;
+            }
+
+            float speedBuild = 0;
+
+            while (speedBuild < Speed)
+            {
+                defaultVector += (Vector2.left * StepSize).Rotate(DegreeToTarget + (StepCounter++ * DegreeStepSize) + (-Degree / 2));
+
+                if (StepCounter + 1 > DegreeStepLength)
+                {
+                    return defaultVector + (Vector2.left * StepSize).Rotate(DegreeToTarget + (DegreeStepLength * DegreeStepSize) + (-Degree / 2));
+                }
+
+                speedBuild += StepSize;
+            }
+
+            return defaultVector;
+        }
+
+        private void FancyMath()
+        {
+            var lineCenter = (transform.position + Target.transform.position) / 2;
+            float L = Vector3.Distance(transform.position, Target.transform.position);
+            float X = (L) / (Mathf.Tan((Degree / 2) * Mathf.Deg2Rad) * 2);
+            float Xdiff = transform.position.x - Target.transform.position.x;
+            float Ydiff = transform.position.y - Target.transform.position.y;
+
+            DegreeToTarget = Mathf.Atan2(Ydiff, Xdiff) * Mathf.Rad2Deg;
+
+            var offset = (Vector2.right * X).Rotate(-90 + DegreeToTarget);
+            var circleCenter = lineCenter + (Vector3)offset;
+            float radius = Vector3.Distance(circleCenter, transform.position);
+            float arcLength = radius * Degree * Mathf.Deg2Rad;
+
+            DegreeStepLength = arcLength / StepSize;
+
+            DegreeStepSize = Degree / DegreeStepLength;
+            StepCounter = 0;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (HideGizmo)
+            {
+                return;
+            }
+            float TotalDegree = Degree;
+
+            //line between
+            Gizmos.DrawLine(transform.position, Target.transform.position);
+
+            //center point 
+            var lineCenter = (transform.position + Target.transform.position) / 2;
+            Gizmos.DrawSphere(lineCenter, 0.1f);
+
+
+            //Cicrle center 
+            float L = Vector3.Distance(transform.position, Target.transform.position);
+            float X = (L) / (Mathf.Tan((TotalDegree / 2) * Mathf.Deg2Rad) * 2);
+
+            float Xdiff = transform.position.x - Target.transform.position.x;
+            float Ydiff = transform.position.y - Target.transform.position.y;
+
+            float degree = Mathf.Atan2(Ydiff, Xdiff) * Mathf.Rad2Deg;
+
+            var offset = (Vector2.right * X).Rotate(-90 + degree);
+
+            //[important]
+            var circleCenter = lineCenter + (Vector3)offset;
+            Gizmos.DrawSphere(circleCenter, 0.1f);
+
+
+            float radius = Vector3.Distance(circleCenter, transform.position);
+
+            //length 
+            float arcLength = radius * TotalDegree * Mathf.Deg2Rad;
+
+            float degreeStepCount = (int)(Mathf.Ceil((float)(arcLength / StepSize)));
+
+            float degreeStep = TotalDegree / degreeStepCount;
+
+            Vector2 from = transform.position;
+            Vector2 to;
+            for (int i = 0; i < degreeStepCount && i < 1000000; i++)
+            {
+                to = from + (Vector2.up * (float)StepSize).Rotate(90 + (degree) + (i * degreeStep) + (-Degree / 2));
+                Gizmos.DrawLine(from, to);
+                from = to;
+            }
+        }
+    }
+
+}
