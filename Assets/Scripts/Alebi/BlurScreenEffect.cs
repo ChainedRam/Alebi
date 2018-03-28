c264c8f30265699e3f6945446b0dcce99ee3d3a2
KLD <KLD0093@hotmail.com>
2018-03-28 16:24:22 +0300

    Fixed blur effect reseting everytime a new sand projectile is fired.
    - Removed BlurStatusEffect from projectile as it's 'Start' method was disabling blur effect.

--- Assets/Scripts/Alebi/BlurScreenEffect.cs
+++ Assets/Scripts/Alebi/BlurScreenEffect.cs
@@ -8,56 +8,56 @@ using UnityStandardAssets.ImageEffects;
 
 public class BlurScreenEffect : MonoBehaviour, IStatusEffect
 {
-	private static BlurOptimized blur;
+    private static BlurOptimized blur;
 
-	private void Start()
-	{	
-		if (blur == null)
-		{
-			blur = Camera.main.GetComponent<BlurOptimized>() ?? Camera.main.gameObject.AddComponent<BlurOptimized>();
-		}
+    private void Start()
+    {	
+        if (blur == null)
+        {
+            blur = Camera.main.GetComponent<BlurOptimized>() ?? Camera.main.gameObject.AddComponent<BlurOptimized>();
+        }
 
-		blur.downsample = 1;
-		blur.blurIterations = 2;
-		blur.blurSize = 3;
-		blur.enabled = false;
-	}
-	public void Show()
-	{
-		blur.enabled = true;
-		gameObject.SetActive(true);
-	}
+        blur.downsample = 1;
+        blur.blurIterations = 2;
+        blur.blurSize = 3;
+        blur.enabled = false;
+    }
+    public void Show()
+    {
+        blur.enabled = true;
+        gameObject.SetActive(true);
+    }
 
-	public void Hide()
-	{
-		blur.enabled = false;
-		gameObject.SetActive(false);
-	}
+    public void Hide()
+    {
+        blur.enabled = false;
+        gameObject.SetActive(false);
+    }
 
-	#region IStatusEffect Interface Property
-	public string Name
-	{
-		get
-		{
-			return "Blur";
-		}
-	}
-	#endregion
-	#region  IStatusEffect Interface Methods
-	public void Init(Player p)
-	{
-		Show();
-	}
+    #region IStatusEffect Interface Property
+    public string Name
+    {
+        get
+        {
+            return "Blur";
+        }
+    }
+    #endregion
+    #region  IStatusEffect Interface Methods
+    public void Init(Player p)
+    {
+        Show();
+    }
 
-	public void Apply(Player p)
-	{
+    public void Apply(Player p)
+    {
 
-	}
+    }
 
-	public void Revert(Player p)
-	{
-		Hide();
-	} 
-	#endregion
+    public void Revert(Player p)
+    {
+        Hide();
+    } 
+    #endregion
 }
 
