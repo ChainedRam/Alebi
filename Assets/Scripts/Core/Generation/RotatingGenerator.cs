74bb2d5f5c39c216f6537f5602f3ff8bcb0548c1
KLD <kld0093@hotmail.com>
2018-04-04 21:12:31 +0300

    Modified Rotating Generator\n-Replaced TargetAngle with loop count.

---
+++ Assets/Scripts/Core/Generation/RotatingGenerator.cs
@@ -0,0 +1,41 @@
+﻿using ChainedRam.Core.Generation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingGenerator : TimedGenerator
+{
+    public GameObject Target;
+    public int Loops; 
+    
+    public float TargetAngle;
+    private float AngularSpeed;
+    private float CurrentAngle; 
+
+    protected override void OnBegin()
+    {
+        base.OnBegin();
+        CurrentAngle = Target.transform.localEulerAngles.z;
+        TargetAngle = 360 * Loops + CurrentAngle;  
+        AngularSpeed = Time.fixedDeltaTime * TargetAngle / (WaitTime);
+    }
+
+    protected override void OnGenerate(GenerateEventArgs e)
+    {
+        if (Mathf.Abs(TargetAngle) - Mathf.Abs(CurrentAngle) <= Mathf.Abs(AngularSpeed))
+        {
+            CurrentAngle = TargetAngle;
+        }
+        else
+        {
+            CurrentAngle += AngularSpeed; 
+        }
+        
+        Target.transform.localEulerAngles = Vector3.forward * CurrentAngle;
+    }
+
+    protected override bool ShouldGenerate()
+    {
+        return true;  
+    }
+}
